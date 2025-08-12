fdc6e0be59b29a5b255f6dae7a86b7161ddfff48
Hlib Danylov <118921940+Zzorro-Kid@users.noreply.github.com>
2025-08-12 23:06:23 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        backgroundColor: Color.fromARGB(255, 78, 121, 38),
+        body: Center(child: Text('Hello World!')),
+      ),
+    ),
+  );
+}
