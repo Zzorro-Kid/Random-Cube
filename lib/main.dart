af47f68fdb89d973be9c379df3fa32115a4937ec
Hlib Danylov <118921940+Zzorro-Kid@users.noreply.github.com>
2025-08-14 18:13:40 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,5 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [ Colors.limeAccent, Color.fromARGB(255, 39, 154, 158)],
-            ),
-          ),
-        child: const Center(child: Text('Hello World!')),
-      ),
-    ),
-  );
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
 }
